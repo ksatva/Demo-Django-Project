e7e2c40dca750e46f8bc1eaf8992b46e51e57c5e
k.ai <ksrt@outlook.com>
2019-12-30 22:18:07 +0530

    telusko django tutorial: 1. setting up python and django environment. 2. Created dynamic content of Hello {{kishore}}, 3. Created base.html as a default template for all html pages, 4. Addition of two numbers by taking input in a form and displaying the result in the webpage using both GET and POST. NEXT: Model View Template(MVT)...

---
+++ calc/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+
+# from django.http import HttpResponse
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, "home.html", {'name': 'kishore'})
+
+def add(request):
+    """The GET vs POST
+    # The GET method takes the input and displays in the address bar (NOT A SECURE WAY)
+    # Therefore POST (also change in home.html)
+    # Remember to use csrf token in home .html {% csrf_token %}
+
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    """
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    res = val1+val2
+    return render(request, "result.html", {'result': res})
